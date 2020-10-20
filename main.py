07856a00f18ecc13063b3513006489748f8b1636
Jesse Donkervliet <j.j.r.donkervliet@vu.nl>
2020-10-20 21:29:26 +0200

    Use new experiment directory structure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import argparse
-import glob
+import fnmatch
 import os
 import subprocess
 import threading
@@ -8,6 +8,8 @@ from enum import unique, Enum
 from typing import List
 
 import toml
+
+_RESOURCES_DIR_NAME = "resources"
 
 
 @unique
@@ -85,13 +87,37 @@ def kill(node: str, pid: str) -> None:
         run_remotely(node, Command(f"kill -9 {pid}"))
 
 
+def find_resource(path: str, pattern: str, file: bool = True, root_path: str = "/"):
+    assert os.path.isdir(root_path)
+    assert os.path.isdir(path)
+    assert os.path.realpath(path).startswith(os.path.realpath(root_path))
+
+    if file:
+        func = os.path.isfile
+    else:
+        func = os.path.isdir
+    resource_path = os.path.join(path, "resources")
+    if os.path.isdir(resource_path):
+        entries = [f for f in os.listdir(resource_path) if func(os.path.join(resource_path, f))]
+        for entry in entries:
+            if fnmatch.fnmatch(entry, pattern):
+                resource = os.path.realpath(os.path.join(resource_path, entry))
+                return resource
+    if path == root_path:
+        return None
+    return find_resource(os.path.dirname(path), pattern, file, root_path)
+
+
 class Instance(ABC):
 
-    def __init__(self, node: str, experiment_path: str, iteration: int):
+    def __init__(self, node: str, path: str, root_path: str, iteration: int):
+        assert os.path.isdir(path)
+        assert os.path.isdir(root_path)
+        assert os.path.realpath(path).startswith(os.path.realpath(root_path))
         self.node = node
         self.iteration = iteration
-        self.experiment_path = experiment_path
-        self.iteration_dir = os.path.join(self.experiment_path, str(self.iteration))
+        self.path = path
+        self.root_path = root_path
 
     @abstractmethod
     def setup(self):
@@ -112,9 +138,8 @@ class Instance(ABC):
 
 class Opencraft(Instance):
 
-    def __init__(self, node: str, experiment_path: str, iteration: int, jvm_args: List[str]):
-        super().__init__(node, experiment_path, iteration)
-        assert os.path.isdir(experiment_path)
+    def __init__(self, node: str, iteration_path: str, root_path: str, iteration: int, jvm_args: List[str]):
+        super().__init__(node, iteration_path, root_path, iteration)
         self.jvm_args = jvm_args
         self.executable = None
         self.pid = None
@@ -122,15 +147,15 @@ class Opencraft(Instance):
 
     def setup(self):
         # TODO make a function for this, with the required checks.
-        opencraft_matches = glob.glob(os.path.join(self.experiment_path, "../../resources/opencraft*.jar"),
-                                      recursive=False)
-        assert len(opencraft_matches) == 1
-        self.executable = opencraft_matches[0]
+        opencraft = find_resource(self.path, "opencraft*.jar",
+                                  root_path=self.root_path)
+        assert opencraft is not None
+        self.executable = opencraft
         self.opencraft_wd = run_remotely(self.node, Command(f"mktemp -d"), mode=RunMode.OUTPUT)
-        run_remotely(self.node,
-                     Command(
-                         f"cp -r {os.path.join(self.experiment_path, '../../resources/config')} {self.opencraft_wd}"),
-                     debug=True)
+        opencraft_config = find_resource(self.path, "config", file=False,
+                                         root_path=self.root_path)
+        assert os.path.isdir(opencraft_config)
+        run_remotely(self.node, Command(f"cp -r {opencraft_config} {self.opencraft_wd}"), debug=True)
 
     def start(self):
         self.pid = run_remotely(self.node, Command(f"java {' '.join(self.jvm_args)} -jar {self.executable}"),
@@ -142,13 +167,13 @@ class Opencraft(Instance):
 
     def clean(self):
         dyconits_log = os.path.join(self.opencraft_wd, 'dyconits.log')
-        dyconits_log_dst = os.path.join(self.iteration_dir, f"dyconits.{self.node}.log")
+        dyconits_log_dst = os.path.join(self.path, f"dyconits.{self.node}.log")
         run_remotely(self.node, Command(f"[ ! -f {dyconits_log} ] || mv {dyconits_log} {dyconits_log_dst}"))
         player_log = os.path.join(self.opencraft_wd, 'players.log')
-        player_log_dst = os.path.join(self.iteration_dir, f"players.{self.node}.log")
+        player_log_dst = os.path.join(self.path, f"players.{self.node}.log")
         run_remotely(self.node, Command(f"[ ! -f {player_log} ] || mv {player_log} {player_log_dst}"))
         run_remotely(self.node, Command(
-            f"mv {os.path.join(self.opencraft_wd, self.node + '.log')} {os.path.join(self.iteration_dir, 'opencraft.' + self.node + '.log')}"))
+            f"mv {os.path.join(self.opencraft_wd, self.node + '.log')} {os.path.join(self.path, 'opencraft.' + self.node + '.log')}"))
         run_remotely(self.node, Command(f"rm -rf {self.opencraft_wd}"))
 
 
@@ -164,8 +189,9 @@ def to_ib(address: str) -> str:
 
 class Yardstick(Instance):
 
-    def __init__(self, node: str, experiment_path: str, iteration: int, jvm_args: List[str], opencraft_node: str):
-        super().__init__(node, experiment_path, iteration)
+    def __init__(self, node: str, experiment_path: str, root_path: str, iteration: int, jvm_args: List[str],
+                 opencraft_node: str):
+        super().__init__(node, experiment_path, root_path, iteration)
         self.jvm_args = jvm_args
         self.opencraft_node = to_ib(opencraft_node)
         self.yardstick_wd = None
@@ -173,14 +199,13 @@ class Yardstick(Instance):
         self.executable = None
 
     def setup(self):
-        yardstick_matches = glob.glob(os.path.join(self.experiment_path, "../../resources/yardstick*.jar"),
-                                      recursive=False)
-        assert len(yardstick_matches) == 1
-        self.executable = yardstick_matches[0]
+        yardstick = find_resource(self.path, "yardstick*.jar", root_path=self.root_path)
+        assert yardstick is not None
+        self.executable = yardstick
         self.yardstick_wd = run_remotely(self.node, Command(f"mktemp -d"), mode=RunMode.OUTPUT)
-        run_remotely(self.node, Command(
-            f"cp {os.path.join(self.experiment_path, '../../resources/yardstick.toml')} {self.yardstick_wd}"),
-                     debug=True)
+        yardstick_config = find_resource(self.path, "yardstick.toml", root_path=self.root_path)
+        assert yardstick_config is not None
+        run_remotely(self.node, Command(f"cp {yardstick_config} {self.yardstick_wd}"), debug=True)
 
     def start(self):
         self.thread = run_remotely(self.node,
@@ -194,26 +219,27 @@ class Yardstick(Instance):
 
     def clean(self):
         run_remotely(self.node, Command(
-            f"mv {os.path.join(self.yardstick_wd, self.node + '.log')} {os.path.join(self.iteration_dir, 'yardstick.' + self.node + '.log')}"))
+            f"mv {os.path.join(self.yardstick_wd, self.node + '.log')} {os.path.join(self.path, 'yardstick.' + self.node + '.log')}"))
         workload_dir = os.path.join(self.yardstick_wd, 'workload')
         run_remotely(self.node, Command(
-            f"[ ! -f {workload_dir} ] || mv {workload_dir} {os.path.join(self.iteration_dir, 'yardstick.workload.' + self.node)}"))
+            f"[ ! -f {workload_dir} ] || mv {workload_dir} {os.path.join(self.path, 'yardstick.workload.' + self.node)}"))
         run_remotely(self.node, Command(f"rm -rf {self.yardstick_wd}"))
 
 
 class Pecosa(Instance):
 
-    def __init__(self, node: str, experiment_path: str, iteration: int, opencraft_pid):
-        super().__init__(node, experiment_path, iteration)
+    def __init__(self, node: str, experiment_path: str, root_path: str, iteration: int, opencraft_pid):
+        super().__init__(node, experiment_path, root_path, iteration)
         self.opencraft_pid = opencraft_pid
         self.executable = None
         self.log_file = None
         self.pid = None
 
     def setup(self):
-        pecosa_matches = glob.glob(os.path.join(self.experiment_path, "../../resources/pecosa.py"), recursive=False)
-        assert len(pecosa_matches) == 1
-        self.executable = pecosa_matches[0]
+        pecosa = find_resource(self.path, "pecosa.py", root_path=self.root_path)
+        assert os.path.isfile(pecosa)
+        self.executable = pecosa
+        # TODO make all log files go to /local?
         self.log_file = run_remotely(self.node, Command(f"mktemp -p /local"), mode=RunMode.OUTPUT)
 
     def start(self):
@@ -226,24 +252,44 @@ class Pecosa(Instance):
     def clean(self):
         filename = f"pecosa.{self.node}.log"
         run_remotely(self.node,
-                     Command(f"mv {self.log_file} {os.path.join(self.iteration_dir, filename)}"))
+                     Command(f"mv {self.log_file} {os.path.join(self.path, filename)}"))
 
 
 def run_experiment(path: str, nodes: list, **kwargs) -> None:
     # TODO check the existence of all necessary files and directories before starting the experiment.
     assert len(path) > 0
     assert os.path.isdir(path)
+    exp_group_path = os.path.dirname(path)
+    assert os.path.isdir(exp_group_path)
+
     assert len(nodes) > 0
 
-    config_path = os.path.join(path, "experiment-config.toml")
+    for entry in os.listdir(path):
+        if entry != _RESOURCES_DIR_NAME and os.path.isdir(os.path.join(path, entry)):
+            run_configuration(nodes, os.path.join(path, entry), exp_group_path)
+
+
+def run_configuration(nodes: List[str], configuration_path: str, root_path: str):
+    config_file = find_resource(configuration_path, "experiment-config.toml", root_path=root_path)
+    assert config_file is not None
+    config = toml.load(config_file)
+    num_iterations = config["iterations"]
+    assert num_iterations is not None
+    assert type(num_iterations) is int
+    for i in range(num_iterations):
+        iteration_path = os.path.join(configuration_path, str(i))
+        run_iteration(nodes, iteration_path, root_path, i)
+
+
+def run_iteration(nodes: List[str], path: str, root_path: str, iteration: int) -> None:
+    if not os.path.isdir(path):
+        os.mkdir(path)
+
+    config_path = find_resource(path, "experiment-config.toml", root_path=root_path)
     assert os.path.isfile(config_path)
     config = toml.load(config_path)
     assert max(config["deployment"]["yardstick"]) < len(nodes)
     # TODO support opencraft world folder, through extra resources dir
-
-    experiment_iterations = config["iterations"]
-    assert experiment_iterations is not None
-    assert type(experiment_iterations) is int
     try:
         opencraft_jvm_args = config["opencraft"]["jvm"]
     except KeyError:
@@ -252,45 +298,32 @@ def run_experiment(path: str, nodes: list, **kwargs) -> None:
         yardstick_jvm_args = config["yardstick"]["jvm"]
     except KeyError:
         yardstick_jvm_args = []
-    for i in range(experiment_iterations):
-        run_iteration(i, nodes, path, opencraft_jvm_args, yardstick_jvm_args, config)
+
+    opencraft_node = nodes[0]
+    yardstick_nodes = [node for node in nodes if nodes.index(node) in config["deployment"]["yardstick"]]
+
+    _run_iteration(iteration, opencraft_node, yardstick_nodes, opencraft_jvm_args, yardstick_jvm_args, path, root_path)
 
 
-def run_iteration(iteration: int, nodes: list, path: str,
-                  opencraft_jvm_args: List[str],
-                  yardstick_jvm_args: List[str],
-                  config) -> None:
-    iteration_dir = os.path.join(path, str(iteration))
-    if os.path.isdir(iteration_dir):
-        return
-    else:
-        os.mkdir(iteration_dir)
-
-    node = nodes[0]
-
-    opencraft = Opencraft(node, path, iteration, opencraft_jvm_args)
+def _run_iteration(iteration, opencraft_node, yardstick_nodes, opencraft_jvm_args, yardstick_jvm_args, path, root_path):
+    opencraft = Opencraft(opencraft_node, path, root_path, iteration, opencraft_jvm_args)
     opencraft.setup()
     opencraft.start()
-    pecosa = Pecosa(node, path, iteration, opencraft.pid)
+    pecosa = Pecosa(opencraft_node, path, root_path, iteration, opencraft.pid)
     pecosa.setup()
     pecosa.start()
-
     yardstick_instances = []
-    for index in config["deployment"]["yardstick"]:
-        node = nodes[index]
-        yardstick = Yardstick(node, path, iteration, yardstick_jvm_args, opencraft.node)
+    for node in yardstick_nodes:
+        yardstick = Yardstick(node, path, root_path, iteration, yardstick_jvm_args, opencraft.node)
         yardstick.setup()
         yardstick_instances.append(yardstick)
-
     for yi in yardstick_instances:
         yi.start()
     for yi in yardstick_instances:
         # TODO change call to signal that Yardstick.stop simply waits.
         yi.stop()
-
     pecosa.stop()
     opencraft.stop()
-
     pecosa.clean()
     opencraft.clean()
     for yi in yardstick_instances:
